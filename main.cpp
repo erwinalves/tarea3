dfbb9f516df6e9884fb10eba7c656c6edaf5583e
Gonzalo Rivas <Gonzalo.RivasV@utem.cl>
2017-11-16 13:00:36 -0400

    corriendo la funcion leer archivos txt y almacenando las coordenadas en una matriz float y los triangulos en una matriz entera

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <math.h>
 
+
 using namespace std;
 
 void leerPuntos(float matriz[5000][3]){
@@ -23,9 +24,9 @@ void leerPuntos(float matriz[5000][3]){
    fclose(fichero);
 }
 
-void leerCoordenada(int matriz[5000][2]){
+void leerTriangulos(int matriz[9665][3]){
   FILE *fichero;
-  fichero = fopen("coordenada.txt","r");
+  fichero = fopen("triangulos","r");
   if (fichero==NULL){
     printf( "No se puede abrir el fichero.\n" );
     system("pause");
@@ -41,23 +42,39 @@ void leerCoordenada(int matriz[5000][2]){
    fclose(fichero);
 }
 
-/*void buscarCoordenada (int arrayA[2],int arrayB[2],matriz[5000][2]){
-  int  i,j;
-  for (i = 0; i < 5000; i++) {
-    for (j = 0; j < 2; j++)
-      if (arrayA[0]==matriz[i][0]){
-        arrayB[0]=matriz[i][1];
-        arrayB[1]=matriz[i][2]
-        i=5000;
+/*int buscarCoordenada (float matrizA[9665][3],float matrizB[5000][3]){
+  int  i,j,k;
+  for (i = 0; i < 9665; i++) {
+    for (j = 0; j < 5000; j++){
+      for(k = 0; k < 3; j++){
+        if(matrizA[i][k]==matrizB[j][0]){
+          matrizA[i][1]=matrizB[j][1];
+          matrizA[i][2]=matrizB[j][2];
+        }
       }
-  }esta es una prueba*/
+        
+    }
+  }
+}*/
 
 int main()
 {
-  float puntos[5000][3];
-  leerPuntos(puntos);
-  printf( "Contenido del fichero:\n" );
   int  i,j;
+  int triangulos[9665][3];
+  leerTriangulos(triangulos);
+  //buscarCoordenada (triangulos,puntos);
+  printf( "Contenido del fichero triangulos:\n" );
+  for (i = 0; i < 9665; i++) {
+    for (j = 0; j < 3; j++)
+      printf ("%d ", triangulos[i][j]);
+      printf ("\n");
+  }
+  float puntos[5000][3];
+
+  leerPuntos(puntos);
+
+  printf( "Contenido del fichero Puntos:\n" );
+  
   for (i = 0; i < 5000; i++) {
     for (j = 0; j < 3; j++)
       printf ("%f ", puntos[i][j]);
