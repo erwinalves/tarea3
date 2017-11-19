a03207f0124cc87783e83031b5575c31bbd1d43a
Gonzalo Rivas <Gonzalo.RivasV@utem.cl>
2017-11-19 00:53:46 -0400

    terminada la funcion leer triangulos la cual devuelve el valor de la suma de los perimetros de los triangulos

--- main.cpp
+++ main.cpp
@@ -7,16 +7,14 @@
 using namespace std;
 using std::setw;
 
-void calcularDistancia (float a[2], float b[2], float c[2], float y){
-  {
-   cout <<"\n\nEste programa calcula la distancia entre dos puntos en el plano cartesiano. "<<endl;
-   
-   y = sqrt((a[1] - b[1])*(a[1] - b[1] ) + (a[2] - b[2])*(a[2] - b[2]));
-
-   cout <<"\nLa distancia entre los puntos es: " << setw(10) <<y << endl;
-
-   
-   }            /* Cierra main */
+float calcularDistancia (float a[2], float b[2], float c[2], float y){
+  y = y + sqrt((b[0] - a[0])*(b[0] - a[0] ) + (b[1] - a[1])*(b[1] - a[1]));
+  //printf("El distancia es : %f \n", y);
+  y = y + sqrt((c[0] - b[0])*(c[0] - b[0] ) + (c[1] - b[1])*(c[1] - b[1]));
+  //printf("El PERIMETRO es : %f \n", y);
+  y = y + sqrt((a[0] - c[0])*(a[0] - c[0] ) + (a[1] - c[1])*(a[1] - c[1]));
+  //printf("El PERIMETRO es : %f \n", y);
+  return y;
 }
 
 void leerPuntos(float matriz[5000][3]){
@@ -37,7 +35,7 @@ void leerPuntos(float matriz[5000][3]){
    fclose(fichero);
 }
 
-void leerTriangulos(int x, float matriz[5000][3]){
+void leerTriangulos(float x, float matriz[5000][3]){
   FILE *fichero;
   fichero = fopen("triangulos","r");
   if (fichero==NULL){
@@ -50,63 +48,30 @@ void leerTriangulos(int x, float matriz[5000][3]){
       break;
     int a,b,c;
     fscanf (fichero, "%d %d %d\n", &a, &b, &c);
+    //printf("Los puntos son : %d %d %d\n", a,b,c);
     float A[2],B[2],C[2];
-    A[0]=matriz[a][1];
-    A[1]=matriz[a][2];
-    B[0]=matriz[b][1];
-    B[1]=matriz[b][2];
-    C[0]=matriz[c][1];
-    C[1]=matriz[c][2];
+    A[0]=matriz[a-1][1];
+    //printf("El X1 es : %f \n", A[0]);
+    A[1]=matriz[a-1][2];
+    //printf("El Y1 es : %f \n", A[1]);
+    B[0]=matriz[b-1][1];
+    //printf("El X2 es : %f \n", B[0]);
+    B[1]=matriz[b-1][2];
+    //printf("El Y2 es : %f \n", B[1]);
+    C[0]=matriz[c-1][1];
+    //printf("El X3 es : %f \n", C[0]);
+    C[1]=matriz[c-1][2];
+    //printf("El Y3 es : %f \n", C[1]);
+    x = calcularDistancia(A, B, C, x);
     i++;
    }
    fclose(fichero);
+   printf("La suma de todos los perimetros es : %f \n", x);
 }
-
-/*int buscarCoordenada (float matrizA[9665][3],float matrizB[5000][3]){
-  int  i,j,k;
-  for (i = 0; i < 9665; i++) {
-    for (j = 0; j < 5000; j++){
-      for(k = 0; k < 3; j++){
-        if(matrizA[i][k]==matrizB[j][0]){
-          matrizA[i][1]=matrizB[j][1];
-          matrizA[i][2]=matrizB[j][2];
-        }
-      }
-        
-    }
-  }
-}*/
-
-int main()
-{
-  float  j=0;
-  float a[2],b[2],c[2],d[2];
-  for(int i=0;i<2;i++){
-    a[i]=(3+i)*i;
-    b[i]=((2+i)*i)-3;
-    c[i]=(6-i)+i*3;
-  }
-  calcularDistancia(a, b, c, j);
-
-  /*int triangulos[9665][3];
-  leerTriangulos(triangulos);
-  //buscarCoordenada (triangulos,puntos);
-  printf( "Contenido del fichero triangulos:\n" );
-  for (i = 0; i < 9665; i++) {
-    for (j = 0; j < 3; j++)
-      printf ("%d ", triangulos[i][j]);
-      printf ("\n");
-  }
+int main(){
   float puntos[5000][3];
-
   leerPuntos(puntos);
-
-  printf( "Contenido del fichero Puntos:\n" );
-  
-  for (i = 0; i < 5000; i++) {
-    for (j = 0; j < 3; j++)
-      printf ("%f ", puntos[i][j]);
-      printf ("\n");
-  }*/
+  float perimetro=0;
+  leerTriangulos(perimetro, puntos);
 }
 
