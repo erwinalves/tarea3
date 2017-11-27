486b7eb1f115156b88b9f0b8b5e15626fd27b355
erwinalves <erwinalbo@hotmail.com>
2017-11-26 21:21:11 -0300

    prueba

--- tarea3P.cpp
+++ tarea3P.cpp
@@ -73,46 +73,35 @@ void leerTriangulos(float x, float matriz[5000][3], int li, int lf){
    printf("La suma de todos los perimetros es : %f \n", x);
 }
  
-int main(int argc, char *argv[])
-{
-  
-    MPI_Status estado;
-    int status,rank_actual,cant_proce;
-    int limite[2];
-    float puntos[5000][3];
-      MPI_Init(&argc, &argv);  // Inicio de MPI   
-      MPI_Comm_size(MPI_COMM_WORLD, &cant_proce);  // numero de procesadores 
-      MPI_Comm_rank(MPI_COMM_WORLD, &rank_actual); // rango de procesadores 
-      if(rank_actual == 0){//procesador 0 comienza a trabajar
-        /*cout<<"Los numeros primos son:"<<endl;
-        //if(numero%2==0){//verificamos si el numero ingresado es par, ya que el limite superior tiene que cambiar para los numeros impares por el tema de los decimales
-          for(int i=0; i<cant_proce; i++){
-            limite[0] = ((numero/cant_proce)*i)+1;//limite inferior (dependiendo de la cantidad de proce dividimos los numeros a procesar) el uno no es primo
-            limite[1] = ((numero/cant_proce)*(i+1));//limite superior 
-            status = MPI_Send(limite, 2, MPI_INT, i, 0, MPI_COMM_WORLD);//envio limites
-          }
-        }
-        else{*/
-          for(int i=0; i<cant_proce; i++){
-            leerPuntos(puntos);
-            limite[0] = ((9665/cant_proce)*i);//limite inferior (dependiendo de la cantidad de proce dividimos los numeros a procesar) 
-            limite[1] = ((9665/cant_proce)*(i+1));//limite superior 
-            status = MPI_Send(limite, 2, MPI_INT, i, 0, MPI_COMM_WORLD);//envio limites
-            status = MPI_Send(&puntos[5000][3], 5000*3, MPI_INT, i, 0, MPI_COMM_WORLD);
-          }
-        //}
-        
-        status = MPI_Recv(limite, 2, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &estado);//recibo limite para el proce 0
-        status = MPI_Recv(&puntos[5000][3], 5000*3, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &estado);
-        float perimetro=0;
-        leerTriangulos(perimetro, puntos, limite[0],limite[1]);
-      }
-      else{//para los demás procesadores 
-        status = MPI_Recv(limite, 2, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &estado);//recibo limite para el proce 0
-        status = MPI_Recv(&puntos[5000][3], 5000*3, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &estado);
-        float perimetro=0;
-        leerTriangulos(perimetro, puntos, limite[0],limite[1]);
-      }
-      MPI_Finalize();
-    //}
+int main(int argc, char *argv[]){
+  MPI_Status estado;
+  int status,rank_actual,cant_proce;
+  int limite[2];
+  float puntos[5000][3];
+
+  MPI_Init(&argc, &argv);  // Inicio de MPI   
+  MPI_Comm_size(MPI_COMM_WORLD, &cant_proce);  // numero de procesadores 
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank_actual); // rango de procesadores 
+
+  if(rank_actual == 0){//procesador 0 comienza a trabajar
+    for(int i=0; i<cant_proce; i++){
+      leerPuntos(puntos);
+      limite[0] = ((9665/cant_proce)*i);//limite inferior (dependiendo de la cantidad de proce dividimos los numeros a procesar) 
+      limite[1] = ((9665/cant_proce)*(i+1));//limite superior 
+      status = MPI_Send(limite, 2, MPI_INT, i, 0, MPI_COMM_WORLD);//envio limites
+      status = MPI_Send(&puntos[5000][3], 5000*3, MPI_INT, i, 0, MPI_COMM_WORLD);
+    }
+
+    status = MPI_Recv(limite, 2, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &estado);//recibo limite para el proce 0
+    status = MPI_Recv(&puntos[5000][3], 5000*3, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &estado);
+    float perimetro=0;
+    leerTriangulos(perimetro, puntos, limite[0],limite[1]);
+  }
+  else{//para los demás procesadores 
+    status = MPI_Recv(limite, 2, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &estado);//recibo limite para el proce 0
+    status = MPI_Recv(&puntos[5000][3], 5000*3, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &estado);
+    float perimetro=0;
+    leerTriangulos(perimetro, puntos, limite[0],limite[1]);
+  }
+  MPI_Finalize();
 }
